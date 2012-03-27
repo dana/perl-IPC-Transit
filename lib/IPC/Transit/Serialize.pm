3002ef92802ebbe5179e972fa32826d55050196b
Dana M. Diederich <diederich@gmail.com>
2012-03-26 19:20:15 -0700

    wip

--- lib/IPC/Transit/Serialize.pm
+++ lib/IPC/Transit/Serialize.pm
@@ -1,6 +1,27 @@
-package IPC::Transit;
+package IPC::Transit::Serialize;
 
 use strict;use warnings;
 
+sub
+freeze {
+    my %args;
+    {   my @args = @_;
+        die 'IPC::Transit::Serialize::freeze: even number of arguments required'
+            if scalar @args % 2;
+        %args = @args;
+    }
+
+}
+
+sub
+thaw {
+    my %args;
+    {   my @args = @_;
+        die 'IPC::Transit::Serialize::thaw: even number of arguments required'
+            if scalar @args % 2;
+        %args = @args;
+    }
+
+}
 
 1;
