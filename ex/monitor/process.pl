fa5fdb3603a98fea52db5fc93bb95c04cb3f5fdf
Dana M. Diederich <diederich@gmail.com>
2012-05-03 07:55:09 -0700

    another stage in the example pipeline

---
+++ ex/monitor/process.pl
@@ -0,0 +1,15 @@
+#!perl
+
+use common::sense;
+use IPC::Transit;
+use IPC::Transit::Test::Example qw(recur);
+use Data::Dumper;
+
+recur(repeat => 1, work => sub {
+    while(my $m = IPC::Transit::receive(qname => 'process', nonblock => 1)) {
+        say $m->dump;
+    }
+});
+
+POE::Kernel->run();
+
