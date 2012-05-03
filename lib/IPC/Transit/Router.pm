5628f3d8e9d3d469ed3d3fb85d7fd3cb8c070195
Dana M. Diederich <diederich@gmail.com>
2012-05-03 10:53:59 -0700

    early version

--- lib/IPC/Transit/Router.pm
+++ lib/IPC/Transit/Router.pm
@@ -1,3 +1,21 @@
+package IPC::Transit::Router;
+
+use strict;use warnings;
+use IPC::Transit;
+
+use vars qw(
+    $config
+);
+sub
+config {
+    $config = shift;
+}
+
+sub
+route {
+
+}
+
 1;
 
 __END__
@@ -5,3 +23,18 @@ __END__
 
 README
 
+{   routes => [
+        {   match => {
+            },
+            forwards => [
+                {   qname => $q1,
+                    hostname => $h1,
+                    continue => [0|1],
+                }
+            ],
+            changes => [
+
+            ],
+        },
+    ]
+}
