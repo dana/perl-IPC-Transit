1eb174bfb1b38d87b82ab6348e17d948ccc3e80e
Dana M. Diederich <diederich@gmail.com>
2012-05-03 07:19:05 -0700

    example1 working well, but found recv is non-blocking

--- ex/monitor/gather.pl
+++ ex/monitor/gather.pl
@@ -1,7 +1,23 @@
 #!env perl
 
-use strict; use warnings;
+use common::sense;
 use IPC::Transit;
 use IPC::Transit::Test::Example qw(recur);
+use File::Slurp;
 
+recur(repeat => 1, work => sub {
+    my $text = read_file('/proc/loadavg') or die 'nothing in /proc/loadavg';
+    if($text =~ /^.*?\s+.*?\s+.*?\s+(?<in_run_queue>\d+)\/(?<total_procs>\d+)/) {
+        IPC::Transit::send(qname => 'process', message => \%+);
+    } else {
+        die 'regex match failed';
+    }
+});
+
+POE::Kernel->run();
+
+__END__
+
+1.36 1.14 0.79 2/385 6792
+2/385 number of processes in run queue / total number of procs
 
